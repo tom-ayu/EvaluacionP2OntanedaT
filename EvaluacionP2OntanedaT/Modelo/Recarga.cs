d07e299f559c3d8bab056617ace4c84c46f9dbc8
tom-ayu <tomas.ontaneda@udla.edu.ec>
2024-12-02 08:56:11 -0500

    Finalización Primera Página: Recarga Telefónica

---
+++ EvaluacionP2OntanedaT/Modelo/Recarga.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluacionP2OntanedaT.Modelo
+{
+    internal class Recarga
+    {
+        public string Numero {  get; set; }
+        public string Nombre { get; set; }
+    }
+}
--- EvaluacionP2OntanedaT/PrimeraPagina.xaml.cs
+++ EvaluacionP2OntanedaT/PrimeraPagina.xaml.cs
@@ -1,9 +1,63 @@
+using EvaluacionP2OntanedaT.Modelo;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
 namespace EvaluacionP2OntanedaT;
 
 public partial class PrimeraPagina : ContentPage
 {
-	public PrimeraPagina()
+	Recarga recarga = new Recarga();
+    public PrimeraPagina()
 	{
 		InitializeComponent();
-	}
+
+        recarga = DevuelveRecarga();
+        BindingContext = recarga;
+    }
+
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "recarga.txt");
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+
+        Recarga recarga = new Recarga
+        {
+            Numero = OntanedaTNum.Text,
+            Nombre = OntanedaTNom.Text
+        };
+
+        bool guardar_recarga = CrearRecarga(recarga);
+
+        if (guardar_recarga)
+        {
+            await DisplayAlert("La recarga fue exitosa", "Recarga exitosa.", "OK");
+        }
+        else
+        {
+            await DisplayAlert("Recarga no se efectuó", "Recarga fallida.", "Fallo");
+        }
+    }
+
+    private bool CrearRecarga(Recarga recarga)
+    {
+        try
+        {
+            string datos = JsonConvert.SerializeObject(recarga);
+            File.WriteAllText(_fileName, datos);
+            return true;
+        }
+        catch { throw; }
+    }
+
+    private Recarga DevuelveRecarga()
+    {
+        Recarga recarga = new Recarga();
+
+        if (File.Exists(_fileName))
+        {
+            string datos = File.ReadAllText(_fileName);
+            recarga = JsonConvert.DeserializeObject<Recarga>(datos);
+        }
+        return recarga;
+    }
 }
